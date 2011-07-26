7e69271428c2907934f93a547bb7fe84c02c0b71
Selem Delul <selem@selem.im>
2011-07-26 18:46:54 -0400

    Rearrange method positions for logical grouping.

--- lib/bitmouth/client.rb
+++ lib/bitmouth/client.rb
@@ -49,7 +49,7 @@ module BitMouth
       case response.status
       when 201
       when 400
-        raise BitMouth::BadRequest.new(response.message, response)
+        raise BitMouth::BadRequest.new("One or more parameters were invalid or this (phone, networkid) pair is already registered.", response)
       when 502
         raise BitMouth::BadGateway.new(response.message, response)
       else
@@ -57,30 +57,6 @@ module BitMouth
       end
 
       response
-    end
-
-    def create_conference(*args)
-      if args.last.is_a? Hash
-        options = args.pop
-      end
-
-      record = false
-      if options
-        record = options[:record]
-      end
-
-      response = post("/call/", :body => {:networkid => args, :appkey => @api_key, :record => record})
-
-      case response.status
-      when 201
-      when 502
-        raise BitMouth::BadGateway.new(response.message, response)
-      else
-        raise BitMouth::Error.new(response.message, response)
-      end
-
-      response
-
     end
 
     def create_media(network_id)
@@ -130,35 +106,6 @@ module BitMouth
       response
     end
 
-    def media_status(media_id)
-      response = get("/media/#{media_id}", :query => {:appkey => @api_key})
-
-      case response.status
-      when 200,201,202
-      when 204
-        raise BitMouth::NoContent.new(response.message, response)
-      when 404
-        raise BitMouth::NotFound.new(response.message, response)
-      end
-
-      response
-    end
-
-    def add_to_conference(network_id, conference_id)
-      response = post("/conference/#{conference_id}", :body => {:networkid => network_id, :appkey => @api_key})
-
-      case response.status
-      when 200,201,202
-      when 400
-        raise BitMouth::BadRequest.new(response.message, response)
-      when 502
-        raise BitMouth::BadGateway.new(response.message, response)
-      end
-
-      response
-
-    end
-
     def upload_grant_request(media_id)
       response = post("/media/#{media_id}",:body => {:appkey => @api_key, :action => "upload_grant"})
 
@@ -168,6 +115,20 @@ module BitMouth
         raise BitMouth::BadRequest.new(response.message, response)
       when 401
         raise BitMouth::Unauthorized.new(response.message, response)
+      end
+
+      response
+    end
+
+    def media_status(media_id)
+      response = get("/media/#{media_id}", :query => {:appkey => @api_key})
+
+      case response.status
+      when 200,201,202
+      when 204
+        raise BitMouth::NoContent.new(response.message, response)
+      when 404
+        raise BitMouth::NotFound.new(response.message, response)
       end
 
       response
@@ -187,6 +148,45 @@ module BitMouth
       end
 
       response
+    end
+
+    def create_conference(*args)
+      if args.last.is_a? Hash
+        options = args.pop
+      end
+
+      record = false
+      if options
+        record = options[:record]
+      end
+
+      response = post("/call/", :body => {:networkid => args, :appkey => @api_key, :record => record})
+
+      case response.status
+      when 201
+      when 502
+        raise BitMouth::BadGateway.new("The request cannot be fulfilled at this time.", response)
+      else
+        raise BitMouth::Error.new(response.message, response)
+      end
+
+      response
+
+    end
+
+    def add_to_conference(network_id, conference_id)
+      response = post("/conference/#{conference_id}", :body => {:networkid => network_id, :appkey => @api_key})
+
+      case response.status
+      when 200,201,202
+      when 400
+        raise BitMouth::BadRequest.new(response.message, response)
+      when 502
+        raise BitMouth::BadGateway.new(response.message, response)
+      end
+
+      response
+
     end
 
     def close_conference(conference_id)
